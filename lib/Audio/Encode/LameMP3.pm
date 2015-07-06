7002b390803b5d884f62392210a4213e1a6d05e6
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-07-06 10:16:17 +0100

    Add some enums, exception

--- lib/Audio/Encode/LameMP3.pm
+++ lib/Audio/Encode/LameMP3.pm
@@ -2,20 +2,45 @@ use v6;
 use NativeCall;
 
 class Audio::Encode::LameMP3:ver<v0.0.1>:auth<github:jonathanstowe> {
-   class GlobalFlags is repr('CPointer') {
-      sub lame_init() returns GlobalFlags is native('libmp3lame') { * }
-      method new() {
-         lame_init();
-      }
 
-   }
+    class X::LameError is Exception {
+        has Str $.message;
+    }
 
-   has GlobalFlags $!gfp;
+    enum VBR-Mode <Off MT RH ABR MTRH>;
+    enum MPEG-Mode <Stereo JointStereo DualChannel Mono NotSet>;
+    enum PaddingType <No All Adjust>;
 
-   submethod BUILD() {
-       $!gfp = GlobalFlags.new;
-   }
+    class GlobalFlags is repr('CPointer') {
+        sub lame_init() returns GlobalFlags is native('libmp3lame') { * }
+        method new() {
+            lame_init();
+        }
 
+        sub lame_init_params(GlobalFlags) returns int32 is native('libmp3lame') { * }
+
+        method init() {
+            my $rc = lame_init_params(self);
+
+            if $rc != 0 {
+                X::LameError.new(message => "Error initialising parameters").throw;
+            }
+        }
+
+    }
+
+    has GlobalFlags $!gfp;
+
+    sub get_lame_version() returns Str is native('libmp3lame') { * }
+
+    method lame-version() returns Version {
+        my $v = get_lame_version();
+        Version.new($v);
+    }
+
+    submethod BUILD() {
+        $!gfp = GlobalFlags.new;
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
